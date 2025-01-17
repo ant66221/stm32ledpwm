62c992ddc025c6215d9c35c89ad9efaff1a99e83
ant66221 <ant66221@naver.com>
2025-01-17 14:47:33 +0900

    메세지

---
+++ Core/Inc/app.h
@@ -0,0 +1,17 @@
+/*
+ * uart.h
+ *
+ *  Created on: Jan 16, 2025
+ *      Author: 김소연
+ */
+
+#ifndef INC_APP_H_
+#define INC_APP_H_
+
+#include "main.h" //반드시 넣기
+
+//함수의 프로토타입 선언
+void app();
+void SysticCallback();
+
+#endif /* INC_APP_H_ */
